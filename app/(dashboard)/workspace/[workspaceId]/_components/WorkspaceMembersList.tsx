09f87c9199a0224ee3028352abedf2163182860b
Wesley <WesleyOlivier443@gmail.com>
2026-03-31 10:38:40 +0200

    15 partyserver

--- app/(dashboard)/workspace/[workspaceId]/_components/WorkspaceMembersList.tsx
+++ app/(dashboard)/workspace/[workspaceId]/_components/WorkspaceMembersList.tsx
@@ -1,14 +1,56 @@
 "use client";
+import { User } from "@/app/schemas/realtime";
 import { Avatar, AvatarFallback } from "@/components/ui/avatar";
+import { usePresence } from "@/hooks/use-presence";
 import { getAvatar } from "@/lib/get-avatar";
 import { orpc } from "@/lib/orpc";
-import { useSuspenseQuery } from "@tanstack/react-query";
+import { cn } from "@/lib/utils";
+import { useQuery, useSuspenseQuery } from "@tanstack/react-query";
 import Image from "next/image";
+import { useParams } from "next/navigation";
+import { useMemo } from "react";
 
 export function WorkspaceMembersList() {
   const {
     data: { members },
   } = useSuspenseQuery(orpc.channel.list.queryOptions());
+
+  const { data: workspaceData } = useQuery(orpc.workspace.list.queryOptions());
+
+  // const currentUser = useMemo(() => {
+  //   if (!workspaceData?.user) return null;
+
+  //   return {
+  //     id: workspaceData.user.id,
+  //     full_name: workspaceData.user.given_name,
+  //     email: workspaceData.user.email,
+  //     picture: workspaceData.user.picture,
+  //   } satisfies User;
+  // }, [workspaceData?.user]);
+
+  const currentUser = workspaceData?.user
+    ? ({
+        id: workspaceData.user.id,
+        full_name: workspaceData.user.given_name,
+        email: workspaceData.user.email,
+        picture: workspaceData.user.picture,
+      } satisfies User)
+    : null;
+
+  const params = useParams();
+
+  const workspaceId = params.workspaceId;
+
+  const { onlineUsers } = usePresence({
+    room: `workspace-${workspaceId}`,
+    currentUser: currentUser,
+  });
+
+  const onlineUserIds = useMemo(
+    () => new Set(onlineUsers.map((u) => u.id)),
+    [onlineUsers],
+  );
+
   return (
     <div className="space-y-0.5 py-1">
       {members.map((member) => (
@@ -28,6 +70,17 @@ export function WorkspaceMembersList() {
                 {member.full_name?.charAt(0).toUpperCase()}
               </AvatarFallback>
             </Avatar>
+
+            {/* Online/offline status indicator */}
+
+            <div
+              className={cn(
+                "absolute bottom-0 right-0 size-2.5 rounded-full border-2 border-background bg-violet-500",
+                member.id && onlineUserIds.has(member.id)
+                  ? "bg-green-500"
+                  : "bg-gray-400",
+              )}
+            ></div>
           </div>
           <div className="flex-1 min-w-0">
             <p className="text-sm font-medium truncate">{member.full_name}</p>
